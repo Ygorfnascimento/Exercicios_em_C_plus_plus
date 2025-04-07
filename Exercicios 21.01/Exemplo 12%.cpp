21d163d5338ab07707fec0ac7e15f1db6a878c30
Ygor Ferreira <ygorferreiradonascimentosilva@gmail.com>
2025-04-07 14:25:32 -0300

    exercícios do dia 21/01

---
+++ Exercicios 21.01/Exemplo 12%.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int valor1;
+    
+    cout<<"Digite o valor: ";
+    cin>>valor1;
+    
+    cout<<"12% de " << valor1 << " é: " << valor1 * 0.12;
+    
+    return 0;
+}
